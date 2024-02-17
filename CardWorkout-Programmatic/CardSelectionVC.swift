ffd10c0743a8a79f53a2d8e89a437723c08662ee
Artsiom Chekh <qbartem@gmail.com>
2024-02-17 18:36:22 +0300

    Added system style and images.

--- CardWorkout-Programmatic/CardSelectionVC.swift
+++ CardWorkout-Programmatic/CardSelectionVC.swift
@@ -10,9 +10,9 @@ import UIKit
 class CardSelectionVC: UIViewController {
     
     let cardImageView   = UIImageView()
-    let stopButton      = CWButton(backgroundColor: .systemRed, title: "Stop!")
-    let resetButton     = CWButton(backgroundColor: .systemGreen, title: "Reset")
-    let rulesBotton     = CWButton(backgroundColor: .systemBlue, title: "Rules")
+    let stopButton      = CWButton(color: .systemRed, title: "Stop!", systemImageName: "stop.circle")
+    let resetButton     = CWButton(color: .systemGreen, title: "Reset", systemImageName: "arrow.clockwise.circle")
+    let rulesBotton     = CWButton(color: .systemBlue, title: "Rules", systemImageName: "list.bullet")
     
     var cards: [UIImage] = CardDeck.allValues
     var timer: Timer!
